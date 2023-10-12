a87732dc88dbc44e499666e8294fe65a92decea8
Doug Linley <doug.linley@gmail.com>
2023-10-12 07:45:24 -0400

    Asteroids now free themselves after leaving screen

--- asteroid.gd
+++ asteroid.gd
@@ -11,13 +11,14 @@ class_name Asteroid
 @onready var x_speed : int
 @onready var y_speed : int
 
+@onready var can_free := false
+
 func init() -> void:
 	var rng = RandomNumberGenerator.new()
 	x_speed = rng.randf_range(x_speed_min, x_speed_max)
 	y_speed = rng.randf_range(y_speed_min, y_speed_max)
-	pass
 
 
-func run(delta) -> void:
+func run(delta) -> void:		
 	position.x += x_speed * delta
 	position.y += y_speed * delta
--- prefabs/asteroid_lg.gd
+++ prefabs/asteroid_lg.gd
@@ -5,3 +5,7 @@ func _ready():
 	
 func _process(delta):
 	run(delta)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free() # Replace with function body.
